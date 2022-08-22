d58a0bc0263f945a6da19e059d58be1c87c98ec9
Aiden <aiden@aidenwallis.co.uk>
2022-08-22 03:11:41 +0100

    add build command + update webpack for non hashed filenames

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
             loader: "file-loader",
             options: {
               esModule: false,
+              name: "[name].[ext]",
             },
           },
         ],
